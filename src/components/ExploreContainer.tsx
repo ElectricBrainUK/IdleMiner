6bd4435511aa944462bf4b7cbb64a9bb7de05476
will <willbc49@gmail.com>
2021-01-05 17:22:37 +0000

    Try launching from program

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './ExploreContainer.css';
+let child = require('child_process').execFile;
+let executablePath = "../ETHMINER/ethminer.exe";
 
 interface ContainerProps { }
+
+child(executablePath, function(err: any, data: any) {
+    if(err){
+        console.error(err);
+        return;
+    }
+
+    console.log(data.toString());
+});
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>Ready to create an app?!!!!!</strong>
+      <p>Start with Ioni@c <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
 };
