1ea2bf22a7c72b4d306a951b2e92cd06b58d52af
friend <mattgreen.xiii@gmail.com>
2021-01-18 23:52:37 +0000

    UI - Settings text input

---
+++ src/components/EB-Settings-Text-Input.tsx
@@ -0,0 +1,21 @@
+import {IonInput, IonItem, IonLabel} from '@ionic/react';
+import React from 'react';
+import './EB-Settings-Text-Input.css';
+
+interface TextInputProps {
+    label: string,
+    placeholder: string,
+    onChange: any
+}
+
+const EBSettingsTextInput: React.FC<TextInputProps> = (props) => {
+
+    return (
+        <IonItem class={"ion-no-padding"}>
+            <IonLabel style={{maxWidth: "220px", width: "220px"}}>{props.label + ":"}</IonLabel>
+            <IonInput placeholder={props.placeholder} type={"text"} onIonChange={props.onChange}/>
+        </IonItem>
+    );
+};
+
+export default EBSettingsTextInput;
