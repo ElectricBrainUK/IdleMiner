b1cd676a397c701b554a5272ffdc2df09a19a6c9
friend <mattgreen.xiii@gmail.com>
2021-01-18 23:52:23 +0000

    UI - Header

---
+++ src/components/EB-Header.tsx
@@ -0,0 +1,35 @@
+import {IonHeader, IonLabel, IonToolbar} from '@ionic/react';
+import React from 'react';
+import './EB-Header.css';
+import {Link} from "react-router-dom";
+
+interface HeaderProps {
+    mainTitle: string,
+    secondaryTitles: string[]
+}
+
+const EBHeader: React.FC<HeaderProps> = (props) => {
+    const options: any = [];
+
+    props.secondaryTitles.forEach((title: string) => {
+        let clss = "eb-title";
+        if (window.location.pathname === "/" + title){
+           clss += " eb-bold";
+        }
+        options.push(
+            <Link to={"/"+title} className={clss}>{title}</Link>
+        );
+    });
+
+    return (
+        <IonHeader>
+            <IonToolbar class="toolbar">
+                <IonLabel class="eb-title eb-bold">{props.mainTitle}</IonLabel>
+                <div className="eb-header-divider"/>
+                {options}
+            </IonToolbar>
+        </IonHeader>
+    );
+};
+
+export default EBHeader;
