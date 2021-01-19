d0dc0f43b2cdb6ec9b513dd173b4dcb0635df74d
friend <mattgreen.xiii@gmail.com>
2021-01-19 01:49:16 +0000

    UI - Move settings to settings

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Redirect, Route} from 'react-router-dom';
 import {IonApp, IonRouterOutlet} from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 import Dashboard from './pages/Dashboard';
+import Settings from "./pages/Settings";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,6 +29,7 @@ const App: React.FC = () => (
         <IonReactRouter>
             <IonRouterOutlet>
                 <Route path="/Dashboard" component={Dashboard} exact={true}/>
+                <Route path="/Settings" component={Settings} exact={true}/>
                 <Route exact path="/" render={() => <Redirect to="/Dashboard"/>}/>
             </IonRouterOutlet>
         </IonReactRouter>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,14 +2,14 @@ import {IonPage} from '@ionic/react';
 import React from 'react';
 import MiningPage from '../components/MiningPage';
 import EBHeader from "../components/EB-Header";
-import './Dashboard.css';
 
-const Dashboard: React.FC = () => {
+const Settings: React.FC = () => {
     return (
         <IonPage>
             <EBHeader mainTitle={"Mine"} secondaryTitles={["Dashboard", "Settings"]}/>
+            <MiningPage/>
         </IonPage>
     );
 };
 
-export default Dashboard;
+export default Settings;
